7965bab427dd73a8a0293ae01ae8413f748997ee
Your Name <your_netid@cornell.edu>
2022-12-27 20:28:08 -0800

    Initial Commit

---
+++ ToiletMaps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToiletMaps
+//
+//  Created by Zane Neelin on 12/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
